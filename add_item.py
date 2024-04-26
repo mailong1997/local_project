c4134c57076e92c2158e6e76d79de7865f9d2624
MaiLong <mailongkf@gmail.com>
2024-04-26 16:46:36 +0700

    update file

--- add_item.py
+++ add_item.py
@@ -13,3 +13,17 @@ b = array(a, k,x)
 
 for i in range(len(b)):
     print(b[i],end =" ")
+
+# Tạo một folder mới tên new_local tại folder buoi_6
+# B1: Truy cập tới folder buoi_6
+# B2: mkdir <tên file>
+
+# Tạo một cây thư mục
+# mkdir <tên folder_1>/<tên folder_2>
+# Chú ý: nếu folder chưa được tạo thì nó sẽ được tạo 
+
+# cd .. quay lại một folder
+# cd ../.. quay lại 2 folder
+
+
+#del <tên folder>  được sử dụng để xóa folder
